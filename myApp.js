a3f2a2d70366052b51f34cb92acd9b7e927154c1
Taylor Lomax <taylor@wilsonlomax.com>
2023-01-09 00:18:29 -0600

    movingOrder

--- myApp.js
+++ myApp.js
@@ -4,16 +4,16 @@ let app = express();
 let viewsIndex = __dirname + '/views/index.html'
 let publicStyle = __dirname + '/public/style.css'
 
+app.get('/json', (req, res) => {
+  res.json({"message": "Hello World"})
+})
+
 app.get("/", (req, res) => {
   res.sendFile(viewsIndex)
 })
 
 app.use(express.static('/public'), (req, res) => {
   res.sendFile(publicStyle)
-})
-
-app.get('/json', (req, res) => {
-  res.json({"message": "Hello World"})
 })
 
 
