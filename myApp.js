703ddc6bed4c12ccdce6286c3a95bb80537025e8
Taylor Lomax <taylor@wilsonlomax.com>
2023-01-08 23:50:22 -0600

    router

--- myApp.js
+++ myApp.js
@@ -1,5 +1,10 @@
+const { Router } = require('express');
 let express = require('express');
 let app = express();
+
+Router.get("/", function (req, res) {
+  res.send("Hello Router");
+})
 
 
 console.log("Hello World");
