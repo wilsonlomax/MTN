1c8f0395bda6549303079c06af825696c7a5e825
Taylor Lomax <taylor@wilsonlomax.com>
2023-01-09 00:03:19 -0600

    absolutePath

--- myApp.js
+++ myApp.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 let express = require('express');
 let app = express();
+let absolutePath = __dirname + relativePath/File.ext
 
 app.get("/", (req, res) => {
-  res.send("Hello Express")
+  res.send(absolutePath)
 })
 
 
