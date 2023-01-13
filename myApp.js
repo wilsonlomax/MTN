010149c2f50214913f651f0d7f66ea566a751fb8
Taylor Lomax <taylor@wilsonlomax.com>
2023-01-12 22:12:59 -0600

    commits

--- myApp.js
+++ myApp.js
@@ -15,7 +15,7 @@ app.get('/now', (req, res, next) => {
   req.time = new Date().toString()
   next()
 }, (req, res) => {
-  res.send({time: req.time})
+  res.send({time: req.time.tostring()})
 })
 
 app.get('/json', (req, res) => {
