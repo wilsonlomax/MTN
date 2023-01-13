af62d14974ee6f8a9cd0dc26b930b584c7e14642
Taylor Lomax <taylor@wilsonlomax.com>
2023-01-12 21:59:05 -0600

    jsonTime

--- myApp.js
+++ myApp.js
@@ -15,7 +15,7 @@ app.get('/now', function middleware(req, res, next) {
   req.time = new Date().toString()
   next()
 }, (req, res) => {
-  res.json({time: req.time})
+  res.send({time: req.time})
 })
 
 app.get('/json', (req, res) => {
