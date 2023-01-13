e27e8c06068bfaa1462a16b2168a74ed804bd43d
Taylor Lomax <taylor@wilsonlomax.com>
2023-01-12 20:37:21 -0600

    responseUpper

--- myApp.js
+++ myApp.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 let express = require('express');
+let dotenv = require('dotenv').config();
 let app = express();
 let viewsIndex = __dirname + '/views/index.html'
 let publicStyle = __dirname + '/public/style.css'
+let response = 'Hello World'.toUpperCase()
 
 app.get('/json', (req, res) => {
   if(process.env.MESSAGE_STYLE === 'uppercase') {
-    res.json({"message": "HELLO JSON"})
+    res.json({"message": response})
   } 
   else {
   res.json({"message": "Hello json"})
